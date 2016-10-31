13cc74d0e90eba4c871625d4454fe3e45dc5ec87
martaarenas <martilla_311@hotmail.com>
2016-10-31 13:52:39 +0100

    Update manola.cpp

--- travestis/manola.cpp
+++ travestis/manola.cpp
@@ -4,14 +4,14 @@ int main(){
 	cout << "Bueno, mira cuando yo me vine a Madrid" << endl;
 	cout << "a mí, que en paz descanse," << endl;
 	cout << "la Manola, que todos conoceis a la Manola, y ella más," << endl;
-	cout << "la Manola que vivií entonces también conmigo, me dijo" << endl;
+	cout << "la Manola que vivía entonces también conmigo, me dijo" << endl;
 	cout << "veneno, que se te han puesto unos cardenales enormes," << endl;
-	cout << "claro, yo llamé a berenjeno de Barcelona, " << endl;
-	cout << "un cirjano, mira lo que me pasa" << endl;
+	cout << "claro, yo llamé Berenjeno de Barcelona, " << endl;
+	cout << "sí, un cirujano, le dije mira lo que me pasa" << endl;
 	cout << "entonces me mandó cortisona que se llama trigon de pos" << endl;
 	cout << "que ella lo conoce de sobra" << endl;
 	cout << "dice ve, que te lo inyecten" << endl;
-	cout << "precisamente, os podeis enterar por Descorporacion Dermoesttica" << endl;
+	cout << "precisamente, os podeis enterar por Descorporacion Dermoestetica" << endl;
 	cout << "yo me operaba los pechos" << endl;
 	cout << "cuando un cirjuano plastico me vio eso" << endl;
 	cout << "durante seis meses, durante dos veces a la semana" << endl;

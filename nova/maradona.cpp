81a313cc511ba2b3ffabc8d38fb1961d0aeadcaf
martaarenas <martilla_311@hotmail.com>
2016-10-31 14:20:49 +0100

    Update maradona.cpp

--- nova/maradona.cpp
+++ nova/maradona.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
 int main(){
-	cout << "Andaa..., no ibas a dar la cara, furbolista." << endl;
+	cout << "Andaa..., no ibas a dar la cara, fubulista." << endl;
 	cout << "eres, sabes quien erees, Maradona" << endl;
 }

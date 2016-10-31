2b029408721d4443bfb14c5586644ca20ca2fc2f
martaarenas <martilla_311@hotmail.com>
2016-10-31 13:54:23 +0100

    Update rocio.cpp

--- travestis/rocio.cpp
+++ travestis/rocio.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main(){
 	cout << "La Rocío de Sevilla,hace más de 30 años que le puso los pechos" << endl;
-	cout << "la silicona se le fue abajo, que parecía las patas de elfante" << endl;
+	cout << "la silicona se le fue abajo, que parecía las patas de elefante" << endl;
 	cout << "eh que no se podia poner" << endl;
 	cout << "y la pobrecita se la han tenido que quitar en la fe" << endl;
-	cout << "que podeis llamar e informaro en el hostpial la fe" << endl;
-	cout << "le han quitalo los pechos" << endl;
+	cout << "que podeis llamar e informaro en el hospital la fe" << endl;
+	cout << "le han quitao los pechos" << endl;
 	cout << "porque los tiene encangrenaos como la que tiene lepra" << endl;
 	return 0;
 }

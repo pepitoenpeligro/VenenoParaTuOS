99561dae974bb8db07b1666c177659cb511f6fb3
martaarenas <martilla_311@hotmail.com>
2016-10-31 13:57:02 +0100

    Update sevillana.cpp

--- travestis/sevillana.cpp
+++ travestis/sevillana.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main(){
 	cout << "A la Ana la Sevillana, a la Ana la Sevillana," << endl;
 	cout << "no me da miedo de ti maricón, de nunca," << endl;
-	cout << "a la Ana la Sevilla cuando fui a tu casa " << endl;
-	cout << "para que a mi ex novio el italiano le pusieras el mentón" << endl;
-	cout << "que me cobraste 33 mil pesetas" << endl;
+	cout << "a la Ana la Sevilla cuando fui yo a tu casa " << endl;
+	cout << "para que a mi ex novio el andrea el italiano le pusieras el mentón" << endl;
+	cout << "que me cobraste 33 mil pesetas, acuerdate" << endl;
 	cout << "la tenias tumbá en el sofá allí con tres litros" << endl;
 	return 0;
 }

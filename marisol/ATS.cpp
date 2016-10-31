172385863b349c7c27639c62cc6cf4ef5b4611ec
martaarenas <martilla_311@hotmail.com>
2016-10-31 14:02:37 +0100

    Update ATS.cpp

--- marisol/ATS.cpp
+++ marisol/ATS.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main(){
-	cout << "Donde tienes el ATS, donde tienes el ATS, el hasky ...  sebireeeñoo, anda maricon, el perro que era el enfermero. -Qué grasiosa ehhh(Marisol) " << endl;
+	cout << "Donde está el ATS, donde está el ATS, el hasky ...  sebireeeñoo, anda maricon, el perro que era el enfermero. -Qué grasiosa ehhh(Marisol) " << endl;
 	return 0;
 }

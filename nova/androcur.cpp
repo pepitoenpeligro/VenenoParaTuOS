9ea48a8e68543853cf4e4af75bfffb4aa89e8e15
martaarenas <martilla_311@hotmail.com>
2016-10-31 14:05:56 +0100

    Update androcur.cpp

--- nova/androcur.cpp
+++ nova/androcur.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main(){
-	cout << "El androcurs ¿te digo lo que te hace?" << endl;
+	cout << "El androcurs ¿te digo lo único que te hace?" << endl;
 	cout << "Que te debilita la polla, nada más" << endl;
-	cout << "y no te corres, es para eso." << endl;
+	cout << "y no te corres, es para eso. Que no me venga..." << endl;
 	cout << "Callaros, ehh ohhh uhhh, que suelto voz de camionero." << endl;
 	return 0;
 }

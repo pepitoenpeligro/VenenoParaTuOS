a79b5fb1112038411ac5804dd45a3e25cdbf95dc
martaarenas <martilla_311@hotmail.com>
2016-10-31 14:02:05 +0100

    Update amiga.cpp

--- marisol/amiga.cpp
+++ marisol/amiga.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main(){
 	cout << "Yo no he sio amiga nunca nunca en la vida tuya maricón" << endl;
-	cout << "Yo fui en la fantasia y te conoci por ella maricon embustera" << endl;
-	cout << "Yo no he sio amiga nunca tuya. Anda Zo Fea, cartujana." << endl;
+	cout << "Yo fui a Isla Fantasía y te conoci por ella, maricon embustera" << endl;
+	cout << "Yo no he sio amiga nunca tuya. Anda zo Fea, cartujana." << endl;
 }

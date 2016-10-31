d8e71b9012979ee0694f1ff3ee499aa37b6b4ae6
martaarenas <martilla_311@hotmail.com>
2016-10-31 14:05:25 +0100

    Update obesa.cpp

--- nova/obesa.cpp
+++ nova/obesa.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-	cout << "Tu te crees que dice que sa tomao dos pastillas de androcurs" << endl;
+	cout << "Tú te crees que se toma androcurs" << endl;
 	cout << "y dice que le han crecio las tetas" << endl;
 	cout << "le han crecio las tetas de obesa." << endl;
 	return 0;

6d0e3a64d9d2bde9d18e517c91602ca1d430d461
martaarenas <martilla_311@hotmail.com>
2016-10-31 14:09:48 +0100

    Update conosia.cpp

--- nova/conosia.cpp
+++ nova/conosia.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main(){
 	cout << "Mira, no, déjame, que me deje, que me heeiii" << endl;
 	cout << "Mira, sabes lo que te digo Pocahontas, Gerónimo, que te voy a decir en tu cara" << endl;
-	cout << "Yo nunca en la vida te he visto, yo soy conosida mundiar" << endl;
-	cout << "¿Donde eres conocía?, eres conocia de aquí a Vallecah" << endl;
+	cout << "Yo nunca en la vida te he visto, yo soy conocia mundiar" << endl;
+	cout << "¿Donde eres conocía?,tu eres conocia desde aquí a Vallecah" << endl;
 	cout << "Tu eres un maricón como un piano, eres un tio" << endl;
 	cout << "a tomar por culo " << endl;
 	cout << "enseña las tetas que tienes de obesa, petarda" << endl;
@@ -15,6 +15,6 @@ int main(){
 	cout << "ashin mismo" << endl;
 	cout << "que vives del cuento, yo soy ordinaria pero tengo más clase que tú" << endl;
 	cout << "y he-me-he tirado a los tios más guapos del mundo " << endl;
-	cout << "pero tú esa cara de mona de tarzán, ¿a quién te vas a tirar?" << endl;
+	cout << "pero tú con esa cara de mona de tarzán, ¿a quién te vas a tirar?" << endl;
 	return 0;
 }

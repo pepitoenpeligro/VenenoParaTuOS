a37a2846a179a114cf1b42432ccb100e07321a13
martaarenas <martilla_311@hotmail.com>
2016-10-31 14:19:55 +0100

    Update perdon.cpp

--- nova/perdon.cpp
+++ nova/perdon.cpp
@@ -6,9 +6,9 @@ int main(){
 	cout << "Perdón, Gerónimo, Pocahontas, El Paaadreee." << endl;
 	cout << "Mira, tu eres la educada, la fina," << endl;
 	cout << "si eres un gladiadóh maricón," << endl;
-	cout << "en primer lugar te via desí una cosa," << endl;
-	cout << "yo no he cirticao a la bibi anderson," << endl;
-	cout << "yo en el momento que cirtique " << endl;
+	cout << "mira, en primer lugar te via desí una cosa," << endl;
+	cout << "yo no he criticao a la bibi anderson," << endl;
+	cout << "yo en el momento que critique " << endl;
 	cout << "a la bibi anderson, " << endl;
 	cout << "aqui está la maria patiño y todas, pedí perdon aquí" << endl;
 	return 0;

3e77ec0fa07cebedbeb4de76f9fa00f73460221a
martaarenas <martilla_311@hotmail.com>
2016-10-31 13:56:08 +0100

    Update boxer.cpp

--- marisol/boxer.cpp
+++ marisol/boxer.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
-	cout << "Eres una ladrona, na mas que hay que verte la cara" << endl;
+	cout << "Eres una ladrona,  hay que verte la cara" << endl;
 	cout << "que eres un boxer." << endl;
 	cout << "Has destrozao maricones, solamente por ganar" << endl;
+	cout << "has destrozao maricones" << endl;
 	cout << "y tu casa cuando llegaba, era una perrera" << endl;
 	return 0;
 }

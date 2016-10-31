a60dbdf4d18ae0a5bde9a082764f513ddd8c2907
martaarenas <martilla_311@hotmail.com>
2016-10-31 14:04:01 +0100

    Update fisna.cpp

--- nova/fisna.cpp
+++ nova/fisna.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
 	cout << "Ay, espérate un momento, que se me ha olvidado" << endl;
 	cout << "No sé como ponerme fisna" << endl;
-	cout << "Me quiero poner fisna como la que vino los otros días" << endl;
+	cout << "Me quiero poner fisna como la que vino los otros días que dice" << endl;
 	cout << "Mira me he tomao dos pastillas de androcurs y me ha cambiao la vos." << endl;
 	return 0;
 }

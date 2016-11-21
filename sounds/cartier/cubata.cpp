9e31cb2824c900f6b33dc983cad41a3e793d949d
martaarenas <martilla_311@hotmail.com>
2016-11-21 12:31:16 +0100

    Add files via upload

---
+++ sounds/cartier/cubata.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+void Texto(){
+	cout << "Mira, un güiscaso, un cubata." << endl;
+	cout << "Pa to las pencoletas del coño, y pal maricon de la silicona. " << endl;
+}
+
+void Audio(){
+	system("play -q /usr/local/bin/audiosVenenisticos/sounds/cartier/cubata.mp3 &");
+}
+
+int main(int argc, char* argv[]){
+	
+	if(argc > 1 && (string)argv[1] == "audio"){
+		Audio();
+	}
+
+	if(argc > 1 && (string)argv[1] == "texto"){
+		Texto();
+	}
+
+	if(argc > 2 && (string)argv[1] == "texto" && (string)argv[2] == "audio"){
+		Texto();
+		Audio();
+	}
+
+	if(argc > 2 && (string)argv[1] == "audio" && (string)argv[2] == "texto"){
+		Texto();
+		Audio();
+	}
+
+	return 0;
+}
+
---
+++ sounds/cartier/poderio.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+void Texto(){
+	cout << "Esto es un poderío que tiene la veneno en el coño metío. " << endl;
+}
+
+void Audio(){
+	system("play -q /usr/local/bin/audiosVenenisticos/sounds/cartier/poderio.mp3 &");
+}
+
+int main(int argc, char* argv[]){
+	
+	if(argc > 1 && (string)argv[1] == "audio"){
+		Audio();
+	}
+
+	if(argc > 1 && (string)argv[1] == "texto"){
+		Texto();
+	}
+
+	if(argc > 2 && (string)argv[1] == "texto" && (string)argv[2] == "audio"){
+		Texto();
+		Audio();
+	}
+
+	if(argc > 2 && (string)argv[1] == "audio" && (string)argv[2] == "texto"){
+		Texto();
+		Audio();
+	}
+
+	return 0;
+}
+
